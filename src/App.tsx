aca59b4f64956ea987e7b664cfe0a1bdc9bbd76b
KrisOP <krisortega15@hotmail.es>
2024-07-23 15:36:35 +0000

    agregando disenio de sitio en construccion

--- src/App.tsx
+++ src/App.tsx
@@ -38,9 +38,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <p className="read-the-docs">
+       Sitio en construccion
+      </p>
     </>
   )
 }
